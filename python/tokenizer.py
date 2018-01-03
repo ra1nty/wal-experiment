b633b8b872f9e1b01227a7ebc1a8ea0ffdc698d3
Daddy <ytang.me@gmail.com>
2018-01-03 23:06:59 +0800

    1.3

--- python/tokenizer.py
+++ python/tokenizer.py
@@ -1,41 +1,14 @@
 names = {}
 
-class Expr: pass
-'''
-class FindOp(Expr):
-	def __init__(self, )
-
-
-class BinOp(Expr):
-    def __init__(self,left,op,right):
-        self.type = "binop"
-        self.left = left
-        self.right = right
-        self.op = op
-
-class Number(Expr):
-    def __init__(self,value):
-        self.type = "number"
-        self.value = value
-
-def p_expression_binop(p):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression'''
-
-    p[0] = BinOp(p[1],p[2],p[3])
-'''
-
 class Node:
     def __init__(self,type,children=None,leaf=None):
-         self.type = type
-         if children:
-              self.children = children
-         else:
-              self.children = [ ]
-         self.leaf = leaf
-	 
+        self.type = type
+        if children:
+            self.children = children
+        else:
+            self.children = []
+        self.leaf = leaf
+
 def p_expression_binop(p):
     '''expression : expression PLUS expression
                   | expression MINUS expression
@@ -45,34 +18,34 @@ def p_expression_binop(p):
     p[0] = Node("binop", [p[1],p[3]], p[2])
 
 
-def p_expression_group(p):
+def p_expression_paren(p):
     'expression : LPAREN expression RPAREN'
-    p[0] = p[2]
+    p[0] = Node("paren", [p[2]])
 
 def p_expression_number(p):
     'expression : NUMBER'
-    p[0] = Number(p[1])
-
+    p[0] = Node("number", [], p[1])
 
 def p_statement_assign(p):
     'statement : NAME ASSIGN expression'
     names[p[1]] = p[3]
+
 def p_expression(p):
 	'''expression : elementexpr
 				  | stringexpr'''
 	p[0] = p[1]
 
-def p_expression_paren(p):
-	'expression : LPAREN expression RPAREN'
-	p[0] = p[2]
-
 def p_statement_find(p):
 	'elementexpr : FIND STRING'
-
+    p[0] = Node("find", [], p[2])
 
 def p_statement_fill(p):
-    'statement : FILL elementexpr stringexpr'
-    p[2].send_keys(p[3])
+    'statement : FILL expression stringexpr'
+    p[0] = Node("fill", [p[2]], p[3])
+
+def p_expression_string(p):
+    'stringexpr : STRING'
+    p[0] = p[1]
 
 def p_expression_string_concat(p):
 	'stringexpr : STRING PLUS STRING'
